43ce033e67fa68a36c531a4c895e39bc984557db
rohitcodeclouds2024 <rohit.kumar@codeclouds.com>
2026-02-26 14:42:38 +0530

    route issues fixes

--- routes/pipedrive.php
+++ routes/pipedrive.php
@@ -19,61 +19,58 @@ Route::post('/request_login',      [PipedriveLoginController::class, 'request_lo
 Route::post('/request_verify',     [PipedriveLoginController::class, 'request_verify']);
 Route::post('/resendOtp',          [PipedriveLoginController::class, 'resendOtp']);
 
-Route::middleware(['auth:web'])
-    ->group(function () {
-        // --- Chat / Template ---
-        Route::get('/chat/{contactId}',                    [PipedriveTemplateController::class, 'contactChat']);
-        Route::post('/sendChat',                           [PipedriveTemplateController::class, 'sendChat']);
-        Route::post('/sendMail',                           [PipedriveTemplateController::class, 'sendMail']);
-        Route::post('/getTemplateData',                    [PipedriveTemplateController::class, 'getTemplateData']);
-        Route::post('/saveTemplate',                       [PipedriveTemplateController::class, 'saveTemplate']);
-        Route::post('/deleteTemplate',                     [PipedriveTemplateController::class, 'deleteTemplate']);
+// --- Chat / Template ---
+Route::get('/chat/{contactId}',                    [PipedriveTemplateController::class, 'contactChat']);
+Route::post('/sendChat',                           [PipedriveTemplateController::class, 'sendChat']);
+Route::post('/sendMail',                           [PipedriveTemplateController::class, 'sendMail']);
+Route::post('/getTemplateData',                    [PipedriveTemplateController::class, 'getTemplateData']);
+Route::post('/saveTemplate',                       [PipedriveTemplateController::class, 'saveTemplate']);
+Route::post('/deleteTemplate',                     [PipedriveTemplateController::class, 'deleteTemplate']);
 
-        // --- Login Check ---
-        Route::get('/checkAlreadyLogin',                   [PipedriveLoginController::class, 'checkAlreadyLogin']);
-        Route::get('/logout',                              [PipedriveLoginController::class, 'logout']);
+// --- Login Check ---
+Route::get('/checkAlreadyLogin',                   [PipedriveLoginController::class, 'checkAlreadyLogin']);
+Route::get('/logout',                              [PipedriveLoginController::class, 'logout']);
 
-        // --- Initial Load ---
-        Route::get('/differentDealStatus',                 [PipedriveInitialLoadController::class, 'differentDealStatus']);
-        Route::post('/statusWiseLeadList',                 [PipedriveInitialLoadController::class, 'statusWiseLeadList']);
-        Route::post('/fetchTotalDealData',                 [PipedriveInitialLoadController::class, 'fetchTotalDealData']);
-        Route::get('/asanaStatusList',                     [PipedriveInitialLoadController::class, 'asanaStatusList']);
-        Route::post('/differentQuestionWiseLead',          [PipedriveInitialLoadController::class, 'differentQuestionWiseLead']);
-        Route::post('/allStatusList',                      [PipedriveInitialLoadController::class, 'allStatusList']);
-        Route::post('/listSpecialStatus',                  [PipedriveInitialLoadController::class, 'listSpecialStatus']);
-        Route::post('/leadsNotesList',                     [PipedriveInitialLoadController::class, 'leadsNotesList']);
-        Route::post('/leadsLogsList',                      [PipedriveInitialLoadController::class, 'leadsLogsList']);
-        Route::post('/leadsFilesList',                     [PipedriveInitialLoadController::class, 'leadsFilesList']);
+// --- Initial Load ---
+Route::get('/differentDealStatus',                 [PipedriveInitialLoadController::class, 'differentDealStatus']);
+Route::post('/statusWiseLeadList',                 [PipedriveInitialLoadController::class, 'statusWiseLeadList']);
+Route::post('/fetchTotalDealData',                 [PipedriveInitialLoadController::class, 'fetchTotalDealData']);
+Route::get('/asanaStatusList',                     [PipedriveInitialLoadController::class, 'asanaStatusList']);
+Route::post('/differentQuestionWiseLead',          [PipedriveInitialLoadController::class, 'differentQuestionWiseLead']);
+Route::post('/allStatusList',                      [PipedriveInitialLoadController::class, 'allStatusList']);
+Route::post('/listSpecialStatus',                  [PipedriveInitialLoadController::class, 'listSpecialStatus']);
+Route::post('/leadsNotesList',                     [PipedriveInitialLoadController::class, 'leadsNotesList']);
+Route::post('/leadsLogsList',                      [PipedriveInitialLoadController::class, 'leadsLogsList']);
+Route::post('/leadsFilesList',                     [PipedriveInitialLoadController::class, 'leadsFilesList']);
 
-        // --- Lead Files ---
-        Route::get('/leadfiledownload/{id?}/{name?}',      [PipedriveLeadController::class, 'leadfiledownload']);
-        Route::get('/leadasanafiledownload/{id?}/{name?}', [PipedriveLeadController::class, 'leadasanafiledownload']);
+// --- Lead Files ---
+Route::get('/leadfiledownload/{id?}/{name?}',      [PipedriveLeadController::class, 'leadfiledownload']);
+Route::get('/leadasanafiledownload/{id?}/{name?}', [PipedriveLeadController::class, 'leadasanafiledownload']);
 
-        // --- Individual Lead ---
-        Route::post('/individualLeadData',                 [PipedriveLeadController::class, 'individualLeadData']);
-        Route::post('/shiftLeadStatus',                    [PipedriveLeadController::class, 'shiftLeadStatus']);
-        Route::post('/updateIndividualLead',               [PipedriveLeadController::class, 'updateIndividualLead']);
-        Route::post('/fetchLeadDataEmail',                 [PipedriveLeadController::class, 'fetchLeadDataEmail']);
-        Route::post('/reassignLeadStatus',                 [PipedriveLeadController::class, 'reassignLeadStatus']);
-        Route::post('/addLeadNote',                        [PipedriveLeadController::class, 'addLeadNote']);
-        Route::post('/addLeadFile',                        [PipedriveLeadController::class, 'addLeadFile']);
-        Route::post('/destroyLeadNote',                    [PipedriveLeadController::class, 'destroyLeadNote']);
-        Route::post('/destroyLeadFile',                    [PipedriveLeadController::class, 'destroyLeadFile']);
-        Route::post('/updateIndiLeadData',                 [PipedriveLeadController::class, 'updateIndiLeadData']);
-        Route::post('/updateIndiLeadDataGroup',            [PipedriveLeadController::class, 'updateIndiLeadDataGroup']);
-        Route::post('/updateAdditionalPolicyLead',         [PipedriveLeadController::class, 'updateAdditionalPolicyLead']);
-        Route::post('/deleteAdditionalPolicyLead',         [PipedriveLeadController::class, 'deleteAdditionalPolicyLead']);
-        Route::post('/leadAsanaDetails',                   [PipedriveLeadController::class, 'leadAsanaDetails']);
-        Route::post('/updateleadAsanaDetails',             [PipedriveLeadController::class, 'updateleadAsanaDetails']);
-        Route::post('/getCollaboratorDetails',             [PipedriveLeadController::class, 'getCollaboratorDetails']);
-        Route::post('/updateCollaboratorDetails',          [PipedriveLeadController::class, 'updateCollaboratorDetails']);
-        Route::post('/updateAssignee',                     [PipedriveLeadController::class, 'updateAssignee']);
+// --- Individual Lead ---
+Route::post('/individualLeadData',                 [PipedriveLeadController::class, 'individualLeadData']);
+Route::post('/shiftLeadStatus',                    [PipedriveLeadController::class, 'shiftLeadStatus']);
+Route::post('/updateIndividualLead',               [PipedriveLeadController::class, 'updateIndividualLead']);
+Route::post('/fetchLeadDataEmail',                 [PipedriveLeadController::class, 'fetchLeadDataEmail']);
+Route::post('/reassignLeadStatus',                 [PipedriveLeadController::class, 'reassignLeadStatus']);
+Route::post('/addLeadNote',                        [PipedriveLeadController::class, 'addLeadNote']);
+Route::post('/addLeadFile',                        [PipedriveLeadController::class, 'addLeadFile']);
+Route::post('/destroyLeadNote',                    [PipedriveLeadController::class, 'destroyLeadNote']);
+Route::post('/destroyLeadFile',                    [PipedriveLeadController::class, 'destroyLeadFile']);
+Route::post('/updateIndiLeadData',                 [PipedriveLeadController::class, 'updateIndiLeadData']);
+Route::post('/updateIndiLeadDataGroup',            [PipedriveLeadController::class, 'updateIndiLeadDataGroup']);
+Route::post('/updateAdditionalPolicyLead',         [PipedriveLeadController::class, 'updateAdditionalPolicyLead']);
+Route::post('/deleteAdditionalPolicyLead',         [PipedriveLeadController::class, 'deleteAdditionalPolicyLead']);
+Route::post('/leadAsanaDetails',                   [PipedriveLeadController::class, 'leadAsanaDetails']);
+Route::post('/updateleadAsanaDetails',             [PipedriveLeadController::class, 'updateleadAsanaDetails']);
+Route::post('/getCollaboratorDetails',             [PipedriveLeadController::class, 'getCollaboratorDetails']);
+Route::post('/updateCollaboratorDetails',          [PipedriveLeadController::class, 'updateCollaboratorDetails']);
+Route::post('/updateAssignee',                     [PipedriveLeadController::class, 'updateAssignee']);
 
-        // --- Contact ---
-        Route::post('/updateContact',                      [PipedriveContactController::class, 'updateContact']);
-        Route::post('/removeContact',                      [PipedriveContactController::class, 'removeContact']);
-        Route::post('/addContact',                         [PipedriveContactController::class, 'addContact']);
-        Route::get('/fetchRequiredContactInfo',            [PipedriveContactController::class, 'fetchRequiredContactInfo']);
-        Route::post('/keepEventLog',                       [PipedriveContactController::class, 'keepEventLog']);
-        Route::post('/deleteEventLog',                     [PipedriveContactController::class, 'deleteEventLog']);
-    });
+// --- Contact ---
+Route::post('/updateContact',                      [PipedriveContactController::class, 'updateContact']);
+Route::post('/removeContact',                      [PipedriveContactController::class, 'removeContact']);
+Route::post('/addContact',                         [PipedriveContactController::class, 'addContact']);
+Route::get('/fetchRequiredContactInfo',            [PipedriveContactController::class, 'fetchRequiredContactInfo']);
+Route::post('/keepEventLog',                       [PipedriveContactController::class, 'keepEventLog']);
+Route::post('/deleteEventLog',                     [PipedriveContactController::class, 'deleteEventLog']);
